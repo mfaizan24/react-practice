64b5379a9b31059551a80d5dab5009980da6dbed
mujtabafaizan <63293009+mujtabafaizan@users.noreply.github.com>
2021-06-05 20:46:21 +0530

    made changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 //import ReactStates from './Revision20210601/ReactStates'
 //import ReactProps from './Revision20210601/ReactProps'
-//import Employee from './Revision20210601/FuncComponent'
+import Employee from './Revision20210601/FuncComponent'
 //const element = <ReactStates/>
 //const element = <ReactProps/>
-//const element = <Employee/>
+const element = <Employee/>
 ReactDOM.render(element, document.getElementById("root"))
